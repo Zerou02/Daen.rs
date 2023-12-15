17666e812751801634730b23db2e80b7bc3553cf
Zerou <e.bertgen@gmx.de>
2023-12-15 13:22:15 +0100

    Name changed

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ fn main() -> Result<(), Error> {
     let window = {
         let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
         WindowBuilder::new()
-            .with_title("Hello Pixels")
+            .with_title("Daen")
             .with_inner_size(size)
             .with_min_inner_size(size)
             .build(&event_loop)
